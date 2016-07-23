cea2f7b5aebb14e9e89ce9e6afa723c4093e7845
George <dyedpurple@googlemail.com>
2016-07-23 01:51:12 +0100

    Minor stylistic changes to please Pylint

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ import pygame
 import game_of_life_funcs as gol_funcs
 
 # Define some colours:
-BLACK = ( 0, 0, 0)
-WHITE = ( 255, 255, 255)
-RED = ( 255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 # Define width and height of each square:
-width = 20
-height = 20
+WIDTH = 20
+HEIGHT = 20
 
 # Set margin between sqares:
-margin = 3
+MARGIN = 3
 
 # Initialise pygame:
 pygame.init()
@@ -29,8 +29,8 @@ NUM_ROWS = 20
 board = gol_funcs.initialiseBoard(NUM_ROWS)
 
 # Set height and width of screen:
-size = [463,463]
-screen = pygame.display.set_mode(size)
+SIZE = [463,463]
+SCREEN = pygame.display.set_mode(SIZE)
 
 # Set title of screen:
 pygame.display.set_caption("Game of Life")
@@ -52,24 +52,24 @@ while done == False:
         if event.type == pygame.QUIT:
             done = True
 
-    screen.fill(BLACK)
+    SCREEN.fill(BLACK)
 
     # Draw grid:
-    for i in range(0,NUM_ROWS):
-        for j in range(0,NUM_ROWS):
+    for i in range(0, NUM_ROWS):
+        for j in range(0, NUM_ROWS):
             color = WHITE
             if board[i][j] == 1:
                 color = RED
-            pygame.draw.rect(screen, color, [(margin+width)*j + margin, (margin+height)*i+margin, width, height])
+            pygame.draw.rect(SCREEN, color, [(MARGIN+WIDTH)*j + MARGIN, (MARGIN+HEIGHT)*i+MARGIN, WIDTH, HEIGHT])
 
     # Limit updates to 4 per second
     clock.tick(4)
 
     # Update screen:
     pygame.display.flip()
-                                                  
+
     board = gol_funcs.generate_next_board(board, NUM_ROWS)
     time.sleep(0.05)
 
 pygame.quit()
-        
+
